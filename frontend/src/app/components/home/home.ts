84a78ba9f7dc7bd31f80151a174c72c525d41413
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-05-23 14:55:50 -0500

    Componente Housing-Location

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+   import { Component } from '@angular/core';
+   import { HousingLocation } from '../housing-location/housing-location';
 
-@Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
-})
-export class Home {
-
-}
+   @Component({
+     selector: 'app-home',
+     imports: [HousingLocation],
+     templateUrl: './home.html',
+     styleUrl: './home.css'
+   })
+   export class Home {
+   }
